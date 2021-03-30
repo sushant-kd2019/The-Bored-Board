115800bb89abeecf04777b42ac6ed4f40d2f38d8
sushant-kd2019 <sushant2019@iiitkottayam.ac.in>
2021-03-30 21:07:45 +0530

    largest tech

---
+++ Largest Tech Stack/app.py
@@ -0,0 +1,21 @@
+import wxpython
+import PyGobject
+import Pmw
+import WCK
+import Tix
+import MySQLdb
+import random
+import math 
+import time
+import os
+import turtle
+import json
+import tkinter
+import re
+import pygame
+import pyautogui
+import PIL
+import django
+
+
+print("hello world")
